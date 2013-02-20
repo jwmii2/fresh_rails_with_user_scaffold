39c4f815fd69e90bfd111387feb4abe62b74f413
John Moorhead <jwmii2@yahoo.com>
2013-02-20 14:13:23 -0500

    uploading of files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreshRailsWithUserModelScaffold::Application.config.secret_token = '9303b65af9792f3c86cc1adf6469807821214cf2ffdbfa740992f90e875f5d317b93a660afac97444d80f4b886b9af6c1d5657451047c6317b4fce71b78117e9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FreshRailsWithUserModelScaffold::Application.config.session_store :cookie_store, key: '_fresh_rails_with_user_model_scaffold_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FreshRailsWithUserModelScaffold::Application.config.session_store :active_record_store
